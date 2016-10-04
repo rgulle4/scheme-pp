83c7a573827f308f843582d4e8767d4e6b7584aa
rgulle4 <rgulle4@lsu.edu>
2016-10-04 02:40:10 -0500

    Make test1.scm more interesting by adding quote
    The reference implementation pretty-prints it like this:
    
    ```scheme
      (display "HeLLo, world! ")
      (define dalmatians 101)
      (define leet1 1337)
      (define some-list '(alice bob carol))
      (define another-list '(alice 'bob carol))
      (define (foo the-atom the-list)
          (cond
              ((null? the-list) 0)
              ((list? (car the-list)) (+ (foo the-atom (car the-list)) (foo the-atom (cdr the-list))))
              ((eqv? (car the-list) the-atom) (+ 1 (foo the-atom (cdr the-list))))
              (else (foo the-atom (cdr the-list)))
          )
      )
    ```
    
    The scanner should spit out these tokens:
    
    ```
      LPAREN
      IDENT, name = display
      STRING, stringVal = HeLLo, world!
      RPAREN
      LPAREN
      IDENT, name = define
      IDENT, name = dalmatians
      INT, intVal = 101
      RPAREN
      LPAREN
      IDENT, name = define
      IDENT, name = leet1
      INT, intVal = 1337
      RPAREN
      LPAREN
      IDENT, name = define
      IDENT, name = some-list
      QUOTE
      LPAREN
      IDENT, name = alice
      IDENT, name = bob
      IDENT, name = carol
      RPAREN
      RPAREN
      LPAREN
      IDENT, name = define
      IDENT, name = another-list
      QUOTE
      LPAREN
      IDENT, name = alice
      QUOTE
      IDENT, name = bob
      IDENT, name = carol
      RPAREN
      RPAREN
      LPAREN
      IDENT, name = define
      LPAREN
      IDENT, name = foo
      IDENT, name = the-atom
      IDENT, name = the-list
      RPAREN
      LPAREN
      IDENT, name = cond
      LPAREN
      LPAREN
      IDENT, name = null?
      IDENT, name = the-list
      RPAREN
      INT, intVal = 0
      RPAREN
      LPAREN
      LPAREN
      IDENT, name = list?
      LPAREN
      IDENT, name = car
      IDENT, name = the-list
      RPAREN
      RPAREN
      LPAREN
      IDENT, name = +
      LPAREN
      IDENT, name = foo
      IDENT, name = the-atom
      LPAREN
      IDENT, name = car
      IDENT, name = the-list
      RPAREN
      RPAREN
      LPAREN
      IDENT, name = foo
      IDENT, name = the-atom
      LPAREN
      IDENT, name = cdr
      IDENT, name = the-list
      RPAREN
      RPAREN
      RPAREN
      RPAREN
      LPAREN
      LPAREN
      IDENT, name = eqv?
      LPAREN
      IDENT, name = car
      IDENT, name = the-list
      RPAREN
      IDENT, name = the-atom
      RPAREN
      LPAREN
      IDENT, name = +
      INT, intVal = 1
      LPAREN
      IDENT, name = foo
      IDENT, name = the-atom
      LPAREN
      IDENT, name = cdr
      IDENT, name = the-list
      RPAREN
      RPAREN
      RPAREN
      RPAREN
      LPAREN
      IDENT, name = else
      LPAREN
      IDENT, name = foo
      IDENT, name = the-atom
      LPAREN
      IDENT, name = cdr
      IDENT, name = the-list
      RPAREN
      RPAREN
      RPAREN
      RPAREN
      RPAREN
    
    ```

--- test1.scm
+++ test1.scm
@@ -4,6 +4,8 @@
 (define dAlMatians ; capitalization of identifier should be converted to lowercase
 101 )     (define 
      leet1 1337)
+(define some-list '(alice bob carol))
+(define another-list '(alice 'bob carol))
 (define (foo the-atom the-list) ; this is another comment
 	(cond ((null? the-list)  0) ((list? (car the-list)) (+ (foo the-atom (car the-list)) (foo the-atom (cdr the-list)))) ((eqv? (car the-list) the-atom) (+ 1  (foo the-atom (cdr the-list)))) (else (foo the-atom (cdr the-list)))))
 
